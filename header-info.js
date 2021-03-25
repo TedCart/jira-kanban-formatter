1c259c302c0ae528c57457abac6f22aceada3746
Ted Cart <ted@mackeycorp.com>
2021-03-25 14:49:50 -0400

    Increment to v1.1

--- header-info.js
+++ header-info.js
@@ -2,7 +2,7 @@
 // @namespace    https://openuserjs.org/users/TedCart
 // @name         Mackey JIRA Kanban formatter
 // @copyright    2020, TedCart (https://openuserjs.org/users/TedCart)
-// @version      1.0
+// @version      1.1
 // @license      MIT
 // @description  Reformat Jira for standup meetings
 // @author       You
